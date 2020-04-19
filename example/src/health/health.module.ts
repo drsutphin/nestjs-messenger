192f1c214645fa00a8028732c2c7b44410147499
Simon Emms <simon@simonemms.com>
2020-04-19 15:44:24 +0100

    fix(export): add transport and module opts to the exported providers

--- example/src/health/health.module.ts
+++ example/src/health/health.module.ts
@@ -9,8 +9,10 @@ import { Module } from '@nestjs/common';
 
 /* Files */
 import HealthController from './health.controller';
+import HealthService from './health.service';
 
 @Module({
   controllers: [HealthController],
+  providers: [HealthService],
 })
 export default class HealthModule {}
